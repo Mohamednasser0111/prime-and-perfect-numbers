e0c33c4e308078ad92a39cc3c77bbb34b7a0c4fe
Mohamednasser0111 <114347929+Mohamednasser0111@users.noreply.github.com>
2022-12-21 21:06:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace hello
+{
+    class prime
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("enter num1");
+            double num1 = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("enter num2");
+            double num2 = double.Parse(Console.ReadLine());
+           
+            Console.WriteLine("the perfect numbers :");
+            for (double i = num1; i < num2; i++)
+            {
+
+              
+                double result = 0;
+                for (int j = 1; j < i ; j++)
+                {
+             
+
+                    if (i % j == 0)
+                    {
+
+                        result += j;
+
+                    }
+
+                }
+                if ( result== i)
+
+                    Console.WriteLine(i);
+            }
+
+        }
+
+
+    }
+
+
+}
